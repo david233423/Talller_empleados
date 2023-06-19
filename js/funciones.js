fb4fef9bbadb8d81f59a51bb8e9fc3a25d5a9aab
David Alvear <alveardavid23@gmail.com>
2023-06-19 16:31:42 -0500

    Taller empleados avance 5

--- js/funciones.js
+++ js/funciones.js
@@ -170,6 +170,17 @@ function mostrarImagen(event) {
 	
   }
 
+  function calcular(){
+    let num1, num2;
+    num1= parseInt (document.getElementById('meses').value);
+    num2= parseInt (document.getElementById('salario').value);
+
+    resultado = num1 *num2;
+    
+    document.getElementById("total").value = resultado ;
+
+}
+
   
 
   
